d6946a454cad956f37ec7ce905a10e12ff5643ec
Claudia Aziz <claudiaa@tilli.pro>
2026-01-21 10:14:40 -0500

    refactor(solutions-carousel): TextContentSection styling

--- src/app/home/solutions-carousel/index.tsx
+++ src/app/home/solutions-carousel/index.tsx
@@ -5,6 +5,7 @@ import ClassNames from "embla-carousel-class-names";
 import useEmblaCarousel, { type EmblaOptionsType } from "embla-carousel-react";
 import Image from "next/image";
 import { industries } from "./data";
+import TextContentSection from "./TextContentSection";
 import { useUnidirectionalEmbla } from "./useUnidirectionalEmbla";
 
 export function SolutionsCarousel() {
@@ -21,26 +22,34 @@ export function SolutionsCarousel() {
   useUnidirectionalEmbla(emblaApi);
 
   return (
-    <section className="embla">
-      <div className="embla__viewport" ref={emblaRef}>
-        <div className="embla__container">
-          {industries.map((industry) => (
-            <div
-              className="embla__slide flex flex-col gap-4 items-center justify-center"
-              key={industry.name}
-            >
-              <Image
-                alt={industry.name}
-                className="embla__slide__img"
-                height={600}
-                src={industry.image}
-                width={600}
-              />
-              <p className="font-medium text-sm">{industry.name}</p>
-            </div>
-          ))}
+    <div>
+      <TextContentSection
+        activeIndustry={industries[0]}
+        handleIndustryClick={() => {}}
+      />
+
+      {/* Carousel */}
+      <section className="embla">
+        <div className="embla__viewport" ref={emblaRef}>
+          <div className="embla__container">
+            {industries.map((industry) => (
+              <div
+                className="embla__slide flex flex-col gap-4 items-center justify-center"
+                key={industry.name}
+              >
+                <Image
+                  alt={industry.name}
+                  className="embla__slide__img"
+                  height={600}
+                  src={industry.image}
+                  width={600}
+                />
+                <p className="font-medium text-sm">{industry.name}</p>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 }
